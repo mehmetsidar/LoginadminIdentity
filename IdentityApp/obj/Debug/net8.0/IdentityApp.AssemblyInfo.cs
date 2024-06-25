0085f97e2edc3cb032152ccc6ddb936dba37cb89
Test Kullanıcısı <Test@gmail.com>
2024-06-25 10:04:10 +0300

    Yapıldı

--- IdentityApp/obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
+++ IdentityApp/obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa1cf5a56fb3f15959a12408121b6db64bad90f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f057af9024400e987ec0db876c15a038061e6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
